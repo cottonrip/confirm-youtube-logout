ac0b59a0bc565370306fbc11f064fb26e206feb0
cotton <129889995+cottonrip@users.noreply.github.com>
2024-10-25 21:25:04 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,26 @@
+function showLogoutMessage() {
+    browser.notifications.create({
+        type: "basic",
+        iconUrl: "icon.png",
+        title: "logout attempted",
+        message: "you almost got logged out lol",
+        priority: 2
+    });
+}
 
+browser.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        browser.tabs.sendMessage(details.tabId, { action: "confirmLogout", logoutUrl: details.url });
+        return { cancel: true };
+    },
+    {
+        urls: [
+            "*://accounts.google.com/Logout*",
+            "*://mail.google.com/mail/u/0/?logout*",
+            "*://www.youtube.com/logout*",
+            "*://drive.google.com/logout*",
+            "*://photos.google.com/logout*"
+        ]
+    },
+    ["blocking"]
+);
